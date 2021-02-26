5d57d5b172a897677412d8990d9f1794f29c3bb0
Noeliel <noelieldev@gmail.com>
2021-02-26 21:44:00 +0100

    Old yet functional hacky method of finding all active courses

--- src/main/java/de/uni_stuttgart/it_rex/course/repository/written/CourseRepository.java
+++ src/main/java/de/uni_stuttgart/it_rex/course/repository/written/CourseRepository.java
@@ -1,13 +1,20 @@
 package de.uni_stuttgart.it_rex.course.repository.written;
 
-import de.uni_stuttgart.it_rex.course.domain.enumeration.PUBLISHSTATE;
 import de.uni_stuttgart.it_rex.course.domain.written.Course;
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.convert.QueryByExamplePredicateBuilder;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.persistence.criteria.Predicate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -15,15 +22,15 @@ import java.util.UUID;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface CourseRepository extends JpaRepository<Course, UUID> {
+public interface CourseRepository extends JpaRepository<Course, UUID>,
+    JpaSpecificationExecutor<Course> {
 
     /**
      * Bla.
      *
      * @return List of courses
-     */
-    @Query(value = "SELECT * FROM course WHERE DATE(start_date) <= "
-        + "DATE(NOW()) AND DATE(end_date) >= DATE(NOW())",
+    */
+    @Query(value = "SELECT * FROM course WHERE end_date >= CURRENT_DATE()",
         nativeQuery = true)
     List<Course> findAllActive();
 
@@ -33,10 +40,9 @@ public interface CourseRepository extends JpaRepository<Course, UUID> {
      * @param publishState The publish state to search for.
      * @return List of courses
      */
-    @Query(value = "SELECT * FROM course WHERE DATE(start_date) <= "
-        + "DATE(NOW()) AND DATE(end_date) >= DATE(NOW()) "
-        + "AND publish_state = :publishState",
+    @Query(value = "SELECT * FROM course WHERE end_date >= CURRENT_DATE() "
+        + "AND publish_state = ?1",
         nativeQuery = true)
     List<Course> findAllActiveWithPublishState(@Param("publishState")
-                                               PUBLISHSTATE publishState);
+                                                   String publishState);
 }
