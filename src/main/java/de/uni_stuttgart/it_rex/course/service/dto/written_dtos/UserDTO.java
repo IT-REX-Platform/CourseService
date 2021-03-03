d8b9a71b78f2bf15de21fbf651d45b498ffbf7cd
acraea-x <acraea@arculamea.com>
2021-03-03 13:10:32 +0100

    implement user info endpoint with dto

---
+++ src/main/java/de/uni_stuttgart/it_rex/course/service/dto/written_dtos/UserDTO.java
@@ -0,0 +1,250 @@
+package de.uni_stuttgart.it_rex.course.service.dto.written_dtos;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+import de.uni_stuttgart.it_rex.course.domain.enumeration.COURSEROLE;
+import de.uni_stuttgart.it_rex.course.domain.enumeration.REXROLE;
+
+/**
+ * The DTO for user information
+ */
+public class UserDTO implements Serializable {
+
+    /**
+     * id of user.
+     */
+    private UUID id;
+
+    /**
+     * user name of user.
+     */
+    private String userName;
+
+    /**
+     * name of user.
+     */
+    private String name;
+
+    /**
+     * given name of user.
+     */
+    private String givenName;
+
+    /**
+     * given name of user.
+     */
+    private String familyName;
+
+    /**
+     * email of user.
+     */
+    private String email;
+
+    /**
+     * Rexrole of user.
+     */
+    private REXROLE rexRole;
+
+    /**
+     * courses with roles of user.
+     */
+    private Map<UUID, COURSEROLE> courses;
+
+    /**
+     * Getter.
+     *
+     * @return the id.
+     */
+    public UUID getId() {
+        return id;
+    }
+
+    /**
+     * Setter.
+     *
+     * @param newId the id
+     */
+    public void setId(final UUID newId) {
+        this.id = newId;
+    }
+
+    /**
+     * Getter.
+     *
+     * @return the userName.
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Setter.
+     *
+     * @param newUserName the userName
+     */
+    public void setUserName(final String newUserName) {
+        this.userName = newUserName;
+    }
+
+    /**
+     * Getter.
+     *
+     * @return the givenName.
+     */
+    public String getGivenName() {
+        return givenName;
+    }
+
+    /**
+     * Setter.
+     *
+     * @param newGivenName the givenName
+     */
+    public void setGivenName(final String newGivenName) {
+        this.givenName = newGivenName;
+    }
+
+    /**
+     * Getter.
+     *
+     * @return the familyName.
+     */
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    /**
+     * Setter.
+     *
+     * @param newFamilyName the familyName
+     */
+    public void setFamilyName(final String newFamilyName) {
+        this.familyName = newFamilyName;
+    }
+
+    /**
+     * Getter.
+     *
+     * @return the email.
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Setter.
+     *
+     * @param newEmail the email
+     */
+    public void setEmail(final String newEmail) {
+        this.email = newEmail;
+    }
+
+    /**
+     * Getter.
+     *
+     * @return the name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Setter.
+     *
+     * @param newName the name
+     */
+    public void setName(final String newName) {
+        this.name = newName;
+    }
+
+    /**
+     * Getter.
+     *
+     * @return the RexRole.
+     */
+    public REXROLE getRexRole() {
+        return rexRole;
+    }
+
+    /**
+     * Setter.
+     *
+     * @param newRexRole the RexRole
+     */
+    public void setRexRole(final REXROLE newRexRole) {
+        this.rexRole = newRexRole;
+    }
+
+    /**
+     * Getter.
+     *
+     * @return the courses.
+     */
+    public Map<UUID, COURSEROLE> getCourses() {
+        return courses;
+    }
+
+    /**
+     * Setter.
+     *
+     * @param newCourses the Courses
+     */
+    public void setCourses(final Map<UUID, COURSEROLE> newCourses) {
+        this.courses = newCourses;
+    }
+
+    /**
+     * Equals method.
+     *
+     * @param o the other object.
+     * @return if they are equal
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        UserDTO userDTO = (UserDTO) o;
+        return Objects.equals(getId(), userDTO.getId()) && Objects.equals(getUserName(), userDTO.getUserName())
+                && Objects.equals(getName(), userDTO.getName())
+                && Objects.equals(getGivenName(), userDTO.getGivenName())
+                && Objects.equals(getFamilyName(), userDTO.getFamilyName())
+                && Objects.equals(getEmail(), userDTO.getEmail()) && getRexRole() == userDTO.getRexRole()
+                && Objects.equals(getCourses(), userDTO.getCourses());
+    }
+
+    /**
+     * Hash code method.
+     *
+     * @return the hash code.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getUserName(), getName(), getGivenName(), getFamilyName(), getEmail(),
+                getRexRole(), getCourses());
+    }
+
+    /**
+     * To string method.
+     *
+     * @return the string
+     */
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+            "id=" + id +
+            ", userName='" + userName + '\'' +
+            ", name='" + name + '\'' +
+            ", givenName='" + givenName + '\'' +
+            ", familyName='" + familyName + '\'' +
+            ", email='" + email + '\'' +
+            ", rexRole=" + rexRole +
+            ", courses=" + courses +
+            '}';
+    }
+}
---
+++ src/main/java/de/uni_stuttgart/it_rex/course/web/rest/written/UserInfoResource.java
@@ -0,0 +1,54 @@
+package de.uni_stuttgart.it_rex.course.web.rest.written;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.uni_stuttgart.it_rex.course.domain.enumeration.REXROLE;
+import de.uni_stuttgart.it_rex.course.security.written.RexAuthz;
+import de.uni_stuttgart.it_rex.course.service.dto.written_dtos.UserDTO;
+
+/**
+ * REST controller for managing {@link User}.
+ */
+@RestController
+@RequestMapping("/api")
+public class UserInfoResource {
+    /**
+     * Logger.
+     */
+    private final Logger log = LoggerFactory.getLogger(UserInfoResource.class);
+
+    /**
+     * Entity name.
+     */
+    private static final String ENTITY_NAME = "UserInfo";
+
+    /**
+     * Application name.
+     */
+    @Value("${jhipster.clientApp.name}")
+    private String applicationName;
+
+
+    @GetMapping("/user-info/")
+    public UserDTO getUserInfo() {
+
+        UserDTO userDto = new UserDTO();
+        userDto.setId(RexAuthz.getUserId());
+        userDto.setUserName(RexAuthz.getUserName().orElse(null));
+        userDto.setName(RexAuthz.getName().orElse(null));
+        userDto.setGivenName(RexAuthz.getGivenName().orElse(null));
+        userDto.setFamilyName(RexAuthz.getFamilyName().orElse(null));
+        userDto.setEmail(RexAuthz.getEmail().orElse(null));
+        userDto.setRexRole(RexAuthz.getRexRole());
+        userDto.setCourses(RexAuthz.getCoursesAndRolesOfUser());
+
+        return userDto;
+    }
+
+}
+
