b79e8c98b503a806e8d468a7804a20a4c03bf881
GarondEisenfaust <chr.schr@web.de>
2021-03-05 18:41:44 +0100

    Fixed tests

--- src/test/java/de/uni_stuttgart/it_rex/course/service/mapper/written/ChapterMapperIT.java
+++ src/test/java/de/uni_stuttgart/it_rex/course/service/mapper/written/ChapterMapperIT.java
@@ -1,20 +1,19 @@
 package de.uni_stuttgart.it_rex.course.service.mapper.written;
 
+import de.uni_stuttgart.it_rex.course.CourseServiceApp;
+import de.uni_stuttgart.it_rex.course.config.TestSecurityConfiguration;
 import de.uni_stuttgart.it_rex.course.domain.written_entities.Chapter;
-import de.uni_stuttgart.it_rex.course.domain.written_entities.ContentReference;
 import de.uni_stuttgart.it_rex.course.service.dto.written_dtos.ChapterDTO;
 import de.uni_stuttgart.it_rex.course.utils.written.ChapterUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(classes = {ChapterMapperImpl.class})
+@SpringBootTest(classes = {CourseServiceApp.class, TestSecurityConfiguration.class})
 class ChapterMapperIT {
   private static final UUID UPDATE_ID = UUID.randomUUID();
 
@@ -22,18 +21,15 @@ class ChapterMapperIT {
   private ChapterMapper chapterMapper;
 
   @Test
-  void updateChapterFromChapter() {
+  void updateChapterFromChapterDTO() {
     Chapter toUpdate = ChapterUtil.createChapter();
     ChapterDTO update = new ChapterDTO();
 
     update.setId(UPDATE_ID);
-    update.setTitle(null);
 
     Chapter expected = new Chapter();
     expected.setId(update.getId());
     expected.setTitle(toUpdate.getTitle());
-    expected.setStartDate(toUpdate.getStartDate());
-    expected.setEndDate(toUpdate.getEndDate());
 
     chapterMapper.updateChapterFromChapterDTO(update, toUpdate);
 
@@ -44,36 +40,21 @@ class ChapterMapperIT {
   void toDTO() {
     Chapter chapter = ChapterUtil.createChapter();
     ChapterDTO expected = new ChapterDTO();
+
     expected.setId(chapter.getId());
     expected.setTitle(chapter.getTitle());
-    expected.setStartDate(chapter.getStartDate());
-    expected.setEndDate(chapter.getEndDate());
 
     ChapterDTO result = chapterMapper.toDTO(chapter);
-    assertEquals(expected, result);
+    ChapterUtil.equalsChapterDTO(expected, result);
   }
 
   @Test
   void toEntity() {
     ChapterDTO chapterDTO = ChapterUtil.createChapterDTO();
     Chapter expected = new Chapter();
+
     expected.setId(chapterDTO.getId());
     expected.setTitle(chapterDTO.getTitle());
-    expected.setStartDate(chapterDTO.getStartDate());
-    expected.setEndDate(chapterDTO.getEndDate());
-
-    final List<UUID> contentIds = chapterDTO.getContentReferenceIds();
-    final List<ContentReference> contents =
-        contentIds.stream().map(contentId -> {
-            final UUID id = contentId;
-            ContentReference contentReference = new ContentReference();
-            //     contentReference.setIndex(i);
-            contentReference.setContentId(id);
-            //     contentReference.setChapterId(expected.getId());
-            return contentReference;
-        }).collect(Collectors.toList());
-
-    // expected.setContents(contents);
 
     Chapter result = chapterMapper.toEntity(chapterDTO);
     ChapterUtil.equalsChapter(expected, result);
---
+++ src/test/java/de/uni_stuttgart/it_rex/course/service/mapper/written/ContentReferenceMapperIT.java
@@ -0,0 +1,71 @@
+package de.uni_stuttgart.it_rex.course.service.mapper.written;
+
+import de.uni_stuttgart.it_rex.course.CourseServiceApp;
+import de.uni_stuttgart.it_rex.course.config.TestSecurityConfiguration;
+import de.uni_stuttgart.it_rex.course.domain.written_entities.Chapter;
+import de.uni_stuttgart.it_rex.course.domain.written_entities.ContentReference;
+import de.uni_stuttgart.it_rex.course.service.dto.written_dtos.ContentReferenceDTO;
+import de.uni_stuttgart.it_rex.course.utils.written.ContentReferenceUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = {CourseServiceApp.class, TestSecurityConfiguration.class})
+class ContentReferenceMapperIT {
+  private static final UUID UPDATE_ID = UUID.randomUUID();
+
+  @Autowired
+  private ContentReferenceMapper contentReferenceMapper;
+
+  @Test
+  void updateContentReferenceFromContentReferenceDTO() {
+    ContentReference toUpdate = ContentReferenceUtil.createContentReference();
+    ContentReferenceDTO update = new ContentReferenceDTO();
+
+    update.setId(UPDATE_ID);
+    update.setContentId(null);
+
+    ContentReference expected = new ContentReference();
+    expected.setId(update.getId());
+    expected.setStartDate(toUpdate.getStartDate());
+    expected.setEndDate(toUpdate.getEndDate());
+    expected.setContentId(toUpdate.getContentId());
+
+    contentReferenceMapper.updateContentReferenceFromContentReferenceDTO(update, toUpdate);
+
+    ContentReferenceUtil.equalsContentReference(expected, toUpdate);
+  }
+
+  @Test
+  void toDTO() {
+    ContentReference contentReference = ContentReferenceUtil.createContentReference();
+    ContentReferenceDTO expected = new ContentReferenceDTO();
+
+    expected.setId(contentReference.getId());
+    expected.setStartDate(contentReference.getStartDate());
+    expected.setEndDate(contentReference.getEndDate());
+    expected.setContentId(contentReference.getContentId());
+
+    ContentReferenceDTO result = contentReferenceMapper.toDTO(contentReference);
+    ContentReferenceUtil.equalsContentReferenceDTO(expected, result);
+  }
+
+  @Test
+  void toEntity() {
+    ContentReferenceDTO contentReferenceDTO
+        = ContentReferenceUtil.createContentReferenceDTO();
+    ContentReference expected = new ContentReference();
+
+    expected.setId(contentReferenceDTO.getId());
+    expected.setStartDate(contentReferenceDTO.getStartDate());
+    expected.setEndDate(contentReferenceDTO.getEndDate());
+    expected.setContentId(contentReferenceDTO.getContentId());
+
+    ContentReference result = contentReferenceMapper.toEntity(contentReferenceDTO);
+    ContentReferenceUtil.equalsContentReference(expected, result);
+  }
+}
