edf4fa27ee01bdee4f25b5eb94fe59e5f44e8b8e
PhilippFu <50925541+PhilippFu@users.noreply.github.com>
2021-03-08 13:57:21 +0100

    Created contentTracker endpoints
    Some exceptions missing

--- src/main/java/de/uni_stuttgart/it_rex/course/web/rest/written/ProgressResource.java
+++ src/main/java/de/uni_stuttgart/it_rex/course/web/rest/written/ProgressResource.java
@@ -3,19 +3,21 @@ package de.uni_stuttgart.it_rex.course.web.rest.written;
 import de.uni_stuttgart.it_rex.course.domain.written_entities.Course;
 import de.uni_stuttgart.it_rex.course.security.written.RexAuthz;
 import de.uni_stuttgart.it_rex.course.service.dto.written_dtos.ChapterDTO;
+import de.uni_stuttgart.it_rex.course.service.dto.written_dtos.ContentProgressTrackerDTO;
+import de.uni_stuttgart.it_rex.course.service.dto.written_dtos.ContentReferenceDTO;
 import de.uni_stuttgart.it_rex.course.service.dto.written_dtos.CourseProgressTrackerDTO;
 import de.uni_stuttgart.it_rex.course.service.written.CourseService;
 import de.uni_stuttgart.it_rex.course.service.written.ProgressTrackingService;
+import de.uni_stuttgart.it_rex.course.web.rest.errors.BadRequestAlertException;
+import io.github.jhipster.web.util.HeaderUtil;
 import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -88,5 +90,89 @@ public class ProgressResource {
         CourseProgressTrackerDTO progress =
             progressTrackingService.findCourseProgress(courseId, userId);
         return ResponseUtil.wrapOrNotFound(Optional.of(progress));
+    }
+
+    /**
+     * {@code POST  /content/ : post a new content tracker for the content Item of request user}
+     *
+     * @param contentReferenceDTO DTO of Content Item Reference
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)}
+     *    * and with body of the new content progress tracker, or with status {@code 400 (Bad Request)}
+     *    * if the content progress tracker has already an ID.
+     */
+    @PostMapping("/content/")
+    public ResponseEntity<ContentProgressTrackerDTO> createContentProgress(
+        @RequestBody ContentReferenceDTO contentReferenceDTO
+    ) {
+        log.debug("REST request to post Content Progress for Content Item : {}", contentReferenceDTO.getContentId());
+
+        //if (contentReferenceDTO.getId() == null) {
+        //    throw new BadRequestAlertException(
+        //        "Invalid id", ENTITY_NAME_COURSEPT, "idnull");
+
+        UUID userId = RexAuthz.getUserId();
+        ContentProgressTrackerDTO result =
+            ProgressTrackingService.startContentProgressTracking(contentReferenceDTO.getId(), userId);
+
+        return ResponseEntity.created(new URI("/api/progress/content/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(applicationName,
+                true, ENTITY_NAME_COURSEPT, result.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * {@code PUT  /content/{trackerId}/progress : update the progress of a content item for a specific user}
+     *
+     * @param trackerId of the content item
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with
+     *    * body of the update content tracker,
+     *    * or with status {@code 400 (Bad Request)} if the content tracker is not valid,
+     *    * or with status {@code 500 (Internal Server Error)} if the content tracker couldn't
+     *    * be updated.
+     */
+    @PutMapping("/content/{trackerId}/progress")
+    public ResponseEntity<ContentProgressTrackerDTO> updateContentProgress(
+        @PathVariable final UUID trackerId) {
+        log.debug("REST request to put Content Progress for Content Item : {}",
+            trackerId);
+
+        UUID userId = RexAuthz.getUserId();
+        ContentProgressTrackerDTO progress =
+            progressTrackingService.findContentProgress(trackerId, userId);
+
+        progressTrackingService.updateContentProgress(progress);
+
+        return ResponseEntity.ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName,
+                true, ENTITY_NAME_COURSEPT, progress.getId().toString()))
+            .body(progress);
+    }
+
+    /**
+     * {@code PUT  /content/{trackerId}/complete : set state of a content item of a specific user on complete}
+     *
+     * @param trackerId id of the content tracker
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with
+     *    * body of the updated content tracker,
+     *    * or with status {@code 400 (Bad Request)} if the content tracker is not valid,
+     *    * or with status {@code 500 (Internal Server Error)} if the content tracker couldn't
+     *    * be updated.
+     */
+    @PutMapping("/content/{trackerId}/complete")
+    public ResponseEntity<ContentProgressTrackerDTO> setContentStateComplete(
+        @PathVariable final UUID trackerId) {
+        log.debug("REST request to put Content State on Complete for Content Item : {}",
+            trackerId);
+
+        UUID userId = RexAuthz.getUserId();
+        ContentProgressTrackerDTO state =
+            progressTrackingService.findContentProgress(trackerId, userId);
+
+        progressTrackingService.setContentStateAsComplete(state);
+
+        return ResponseEntity.ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName,
+                true, ENTITY_NAME_COURSEPT, state.getId().toString()))
+            .body(state);
     }
 }
